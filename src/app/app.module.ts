beaca3fe71f3c12563ac64e8cbfb21ae16cdb699
Ana Caroline Ferreira <ana.ferreira@pti.org.br>
2020-03-23 07:42:51 -0300

    exemplo chartjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstChartComponent } from './first-chart/first-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstChartComponent
   ],
   imports: [
     BrowserModule,
